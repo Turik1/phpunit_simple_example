243331f9b604d252b1eeda56bf2397cbaf362224
Artur Wodtschiz <artur.wodtschiz@tecart.de>
2019-10-14 12:50:40 +0200

    Added tests for project_2

---
+++ project_2/Calculator.php
@@ -0,0 +1,13 @@
+<?php
+class Calculator
+{ 
+	public function add($a, $b)
+	{
+		return $a + $b;
+	}
+
+	public function sub($a, $b)
+	{
+		return $a - $b;
+	} 
+}
---
+++ project_2/CalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+require 'Calculator.php';
+ 
+class CalculatorTests extends \PHPUnit\Framework\TestCase
+{
+    private $calculator;
+ 
+    protected function setUp()
+    {
+        $this->calculator = new Calculator();
+    }
+ 
+    protected function tearDown()
+    {
+        $this->calculator = NULL;
+    }
+
+    public function testSub()
+    {
+        $result = $this->calculator->sub(3, 1);
+        $this->assertEquals(2, $result);
+    } 
+}
